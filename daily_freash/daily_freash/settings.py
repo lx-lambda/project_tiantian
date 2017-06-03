df9954bdc33e9bf0b03fff4992b4d0c652795649
lx <429493769@qq.com>
2017-06-03 10:24:57 +0800

    goodsinfo改名前的提交

--- daily_freash/daily_freash/settings.py
+++ daily_freash/daily_freash/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'df_goods',
     'df_cart',
     'df_order',
+    'tinymce',
 
 )
 
@@ -112,3 +113,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'statics'),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': 'advanced',
+    'width': 600,
+    'height': 400,
+}
